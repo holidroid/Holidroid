3972af3c44f5711bb3c61a5160aa0a21e8936a64
holidroid <holidroidlab@gmail.com>
2015-10-07 20:54:07 +0900

    test commit2

--- yahweh87/MyApplication2/app/src/main/java/com/example/etnersrnd/myapplication/MainActivity.java
+++ yahweh87/MyApplication2/app/src/main/java/com/example/etnersrnd/myapplication/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(getApplicationContext(), "This time is Github Test", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "This time is Slack Test", Toast.LENGTH_LONG).show();
+
     }
 
     @Override
